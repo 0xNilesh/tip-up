89f49ba4a447b38b8d57bdc5336e3b794f43e618
aman035 <guptaaman200115@gmail.com>
2024-11-16 19:55:08 +0700

    add: privy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,32 +8,51 @@ import { Web3AuthProvider } from '@web3auth/modal-react-hooks'
 import Login from './pages/Login'
 import TipMangementDashboard from './pages/TipManagementDshboard'
 import TipExplorer from './pages/TipExplorer'
+import { PrivyProvider } from '@privy-io/react-auth'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Web3AuthProvider config={web3AuthContextConfig}>
-        <Router>
-          {/* Theme toggle will be visible on all routes */}
-          <div className="fixed top-4 right-4">
-            <ModeToggle />
-          </div>
-          <Routes>
-            {/* Home page route */}
-            <Route path="/" element={<Home />} />
-            {/* Login page route */}
-            <Route path="/login" element={<Login />} />
-            {/* Dynamic app and username route */}
-            <Route path="/tip/:app/:username" element={<Tip />} />
-            {/* Tip Management Dashboard */}
-            <Route path="/dashboard" element={<TipMangementDashboard />} />
-            {/* Tip Explorer */}
-            <Route path="/explorer" element={<TipExplorer />} />
-            {/* 404 route - Redirect to Home */}
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </Router>
-      </Web3AuthProvider>
+      <PrivyProvider
+        appId="cm3k4hfl602jjwd7shiocem7i"
+        config={{
+          // Display email and wallet as login methods
+          loginMethods: ['email', 'wallet'],
+          // Customize Privy's appearance in your app
+          appearance: {
+            theme: 'light',
+            accentColor: '#676FFF',
+            logo: 'https://your-logo-url',
+          },
+          // Create embedded wallets for users who don't have a wallet
+          embeddedWallets: {
+            createOnLogin: 'users-without-wallets',
+          },
+        }}
+      >
+        <Web3AuthProvider config={web3AuthContextConfig}>
+          <Router>
+            {/* Theme toggle will be visible on all routes */}
+            <div className="fixed top-4 right-4">
+              <ModeToggle />
+            </div>
+            <Routes>
+              {/* Home page route */}
+              <Route path="/" element={<Home />} />
+              {/* Login page route */}
+              <Route path="/login" element={<Login />} />
+              {/* Dynamic app and username route */}
+              <Route path="/tip/:app/:username" element={<Tip />} />
+              {/* Tip Management Dashboard */}
+              <Route path="/dashboard" element={<TipMangementDashboard />} />
+              {/* Tip Explorer */}
+              <Route path="/explorer" element={<TipExplorer />} />
+              {/* 404 route - Redirect to Home */}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Router>
+        </Web3AuthProvider>
+      </PrivyProvider>
     </ThemeProvider>
   )
 }
