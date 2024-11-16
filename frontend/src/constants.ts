948d91497984dd57604c8c25bb62d174d96c4920
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-11-16 13:37:10 +0700

    refactor: added constants

---
+++ frontend/src/constants.ts
@@ -0,0 +1,19 @@
+export const API_URL:string =  'https://tipper-386g.onrender.com/wallet';
+// export const API_URL: string = "http://localhost:3000/wallet";
+
+export const NATIVE_TOKEN_ADDRESS: string = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+
+/**
+ * 1) No Preference -> Every chain
+ * 2) Preference ->
+ *      1. same chain -> 
+ *          a. cow supported -> 
+ *              i. token path available -> use cow
+ *              ii. token path not available -> directly transfer token
+ *          b. cow not supported -> directly transfer token
+ *      2. different chain ->
+ *          a. 1inch not supported on source/destination -> directly transfer token
+ *          b. 1inch supported ->
+ *              i. token path available -> use 1inch
+ *              ii. token path not available -> directly transfer token
+*/
