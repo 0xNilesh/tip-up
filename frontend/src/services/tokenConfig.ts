ff72f7394cb0c1a5b9af96503e3d145bfee07bc6
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-11-16 13:38:54 +0700

    feat: added token config

---
+++ frontend/src/services/tokenConfig.ts
@@ -0,0 +1,275 @@
+export const tokenConfig: {
+  [key: string]: {
+    [token: string]: {
+      address: string;
+      ticker: string;
+      decimals: number;
+    };
+  };
+} = {
+  ethereum: {
+    ETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ETH",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  polygon: {
+    MATIC: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "MATIC",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  base: {
+    ETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ETH",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
+      ticker: "USDT",
+      decimals: 6,
+    },
+    DAI: {
+      ticker: "DAI",
+      decimals: 18,
+      address: "0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb",
+    },
+  },
+  arbitrum: {
+    AETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "AETH",
+      decimals: 18,
+    },
+    USDC: {
+      ticker: "USDC",
+      address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  celo: {
+    CELO: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "CELO",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0xcebA9300f2b948710d2653dD7B07f33A8B32118C",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0x617f3112bf5397D0467D315cC709EF968D9ba546",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  flare: {
+    FLR: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "FLR",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0xFbDa5F676cB37624f28265A144A48B0d6e87d3b6",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0x0B38e83B86d491735fEaa0a791F65c2B99535396",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  scroll: {
+    ETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ETH",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  bitkub: {
+    KUB: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "KUB",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x77071ad51ca93fc90e77BCdECE5aa6F1B40fcb21",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xE5DEC503CD76CE8777cb2FfD8ffe0a15DB10ec38",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  kinto: {
+    ETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ETH",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x05DC0010C9902EcF6CBc921c6A4bd971c69E5A2E",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    kinto: {
+      address: "0x010700808D59d2bb92257fCafACfe8e5bFF7aB87",
+      ticker: "KINTO",
+      decimals: 18,
+    },
+  },
+  gnosis: {
+    XDAI: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "xDAI",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x2a22f9c3b484c3629090FeED35F17Ff8F88f76F0",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0x4ECaBa5870353805a9F068101A40E0f32ed605C6",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  //   hedera: {
+  //     HBAR: {
+  //       address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+  //       ticker: "HBAR",
+  //       decimals: 8,
+  //     },
+  //     USDC: {
+  //       ticker: "USDC",
+  //       decimals: 6,
+  //     },
+  //     USDT: {
+  //       ticker: "USDT",
+  //       decimals: 6,
+  //     },
+  //   },
+  neon: {
+    NEON: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "NEON",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0xEA6B04272f9f62F997F666F07D3a974134f7FFb9",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0x5f0155d08eF4aaE2B500AefB64A3419dA8bB611a",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  rootstock: {
+    RBTC: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "RBTC",
+      decimals: 18,
+    },
+  },
+  oasis: {
+    ROSE: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ROSE",
+      decimals: 18,
+    },
+    ROSY: {
+      address: "0x6665a6Cae3F52959f0f653E3D04270D54e6f13d8",
+      ticker: "ROSY",
+      decimals: 18,
+    },
+    IllumineX: {
+      address: "0x08Fe02Da45720f754e6FCA338eC1286e860d2d2f",
+      ticker: "IX",
+      decimals: 18,
+    },
+  },
+  mantle: {
+    MNT: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "MNT",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x09Bc4E0D864854c6aFB6eB9A9cdF58aC190D0dF9",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0x201EBa5CC46D216Ce6DC03F6a759e8E766e956aE",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+  linea: {
+    ETH: {
+      address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      ticker: "ETH",
+      decimals: 18,
+    },
+    USDC: {
+      address: "0x176211869cA2b568f2A7D4EE941E073a821EE1ff",
+      ticker: "USDC",
+      decimals: 6,
+    },
+    USDT: {
+      address: "0xA219439258ca9da29E9Cc4cE5596924745e12B93",
+      ticker: "USDT",
+      decimals: 6,
+    },
+  },
+};
