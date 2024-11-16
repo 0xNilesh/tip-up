9a1285e5322793ddb3bf10dc569429dd7901f789
Nilesh Gupta <guptanilesh2312@gmail.com>
2024-11-16 13:36:16 +0700

    refactor: added auth0 and web3auth initializations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,39 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ModeToggle } from '@/components/mode-toggle'
 import Home from './pages/Home'
-import AppDetails from './pages/AppDetails'
+import Tip from './pages/Tip'
+import web3AuthContextConfig from './services/web3AuthContext'
+import { Web3AuthProvider } from '@web3auth/modal-react-hooks'
+import Login from './pages/Login'
+import TipMangementDashboard from './pages/TipManagementDshboard'
+import TipExplorer from './pages/TipExplorer'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        {/* Theme toggle will be visible on all routes */}
-        <div className="fixed top-4 right-4">
-          <ModeToggle />
-        </div>
-        <Routes>
-          {/* Home page route */}
-          <Route path="/" element={<Home />} />
-
-          {/* Dynamic app and username route */}
-          <Route path="/app/:app/username/:username" element={<AppDetails />} />
-        </Routes>
-      </Router>
+      <Web3AuthProvider config={web3AuthContextConfig}>
+        <Router>
+          {/* Theme toggle will be visible on all routes */}
+          <div className="fixed top-4 right-4">
+            <ModeToggle />
+          </div>
+          <Routes>
+            {/* Home page route */}
+            <Route path="/" element={<Home />} />
+            {/* Login page route */}
+            <Route path="/login" element={<Login />} />
+            {/* Dynamic app and username route */}
+            <Route path="/tip/:app/:username" element={<Tip />} />
+            {/* Tip Management Dashboard */}
+            <Route path="/dashboard" element={<TipMangementDashboard />} />
+            {/* Tip Explorer */}
+            <Route path="/explorer" element={<TipExplorer />} />
+            {/* 404 route - Redirect to Home */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </Web3AuthProvider>
     </ThemeProvider>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-o7z6ei3bl6iph6qv.us.auth0.com"
+      clientId="NmciuF37ELqq3VkxBBoZ0wjSLXMbSntq"
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/#/dashboard`,
+        audience: 'https://dev-o7z6ei3bl6iph6qv.us.auth0.com/api/v2/',
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>
 )
